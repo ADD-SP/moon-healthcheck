3f08cdf2dd8efc2740c90cc66be1516f28c92f95
Qi <add_sp@outlook.com>
2023-04-02 22:58:53 +0800

    docs(example): fix wrong examples

--- moon-healthcheck/src/lib.rs
+++ moon-healthcheck/src/lib.rs
@@ -7,13 +7,14 @@
 //!     * HTTP GET, POST, PUT, DELETE, HEAD, PATCH
 //!     * HTTP/1.1, HTTP/2
 //! * UDP healthcheck
+//! * Active (background) healthcheck
 //!
 //! # Examples
 //! ## TCP healthcheck
 //! ```rust
 //! use moon_healthcheck::tcp::TcpChecker;
 //! use moon_healthcheck::dispatcher::Dispatcher;
-//! use std::time::Duration;
+//! use tokio::time::Duration;
 //! use tokio::time::sleep;
 //! 
 //! #[tokio::main]
@@ -23,8 +24,27 @@
 //!     let tcp_checker = TcpChecker::new("really.really.not.exists.host", 80, 1);
 //!     let uuid = dispatcher.schedule(tcp_checker, Duration::from_secs(1)).unwrap();
 //!     
-//!     sleep(Duration::from_secs(2)).await;
-//!     assert_eq!(dispatcher.is_healthy(&uuid).await.unwrap(), true);
+//!     sleep(Duration::from_secs(3)).await;
+//!     assert_eq!(dispatcher.is_healthy(&uuid).await.unwrap(), false);
+//! }
+//! ```
+//! 
+//! ## UDP healthcheck
+//! ```rust
+//! use moon_healthcheck::udp::UdpChecker;
+//! use moon_healthcheck::dispatcher::Dispatcher;
+//! use tokio::time::Duration;
+//! use tokio::time::sleep;
+//! 
+//! #[tokio::main]
+//! async fn main() {
+//!     let mut dispatcher: Dispatcher<UdpChecker> = Dispatcher::new();
+//! 
+//!     let udp_checker = UdpChecker::new("really.really.not.exists.host", 53, &vec![1, 2, 3], &vec![1, 2, 3], 1);
+//!     let uuid = dispatcher.schedule(udp_checker, Duration::from_secs(1)).unwrap();
+//! 
+//!     sleep(Duration::from_secs(3)).await;
+//!     assert_eq!(dispatcher.is_healthy(&uuid).await.unwrap(), false);
 //! }
 //! ```
 //! 
@@ -32,7 +52,7 @@
 //! ```rust
 //! use moon_healthcheck::http::HttpChecker;
 //! use moon_healthcheck::dispatcher::Dispatcher;
-//! use std::time::Duration;
+//! use tokio::time::Duration;
 //! use tokio::time::sleep;
 //! 
 //! #[tokio::main]
@@ -42,8 +62,8 @@
 //!     let http_checker = HttpChecker::new("https://really.really.not.exists.host", 1);
 //!     let uuid = dispatcher.schedule(http_checker, Duration::from_secs(1)).unwrap();
 //! 
-//!     sleep(Duration::from_secs(2)).await;
-//!     assert_eq!(dispatcher.is_healthy(&uuid).await.unwrap(), true);
+//!     sleep(Duration::from_secs(3)).await;
+//!     assert_eq!(dispatcher.is_healthy(&uuid).await.unwrap(), false);
 //! }
 //! ```
 //! 
